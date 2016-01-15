45ad963f3759ae2bb77c5f815e4bcfc0cb575c4b
Hook25 <massigi97@gmail.com>
2016-01-15 10:30:07 +0100

    Started developing IDE

---
+++ DODOSembly/IDE/TextEditor.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace IDE {
+    partial class TextEditor {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.splitter = new System.Windows.Forms.SplitContainer();
+            this.txtPointers = new System.Windows.Forms.RichTextBox();
+            this.txtCode = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).BeginInit();
+            this.splitter.Panel1.SuspendLayout();
+            this.splitter.Panel2.SuspendLayout();
+            this.splitter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitter
+            // 
+            this.splitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitter.Location = new System.Drawing.Point(0, 0);
+            this.splitter.Name = "splitter";
+            // 
+            // splitter.Panel1
+            // 
+            this.splitter.Panel1.Controls.Add(this.txtPointers);
+            // 
+            // splitter.Panel2
+            // 
+            this.splitter.Panel2.Controls.Add(this.txtCode);
+            this.splitter.Size = new System.Drawing.Size(583, 310);
+            this.splitter.SplitterDistance = 62;
+            this.splitter.TabIndex = 2;
+            // 
+            // txtPointers
+            // 
+            this.txtPointers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPointers.Location = new System.Drawing.Point(0, 0);
+            this.txtPointers.Name = "txtPointers";
+            this.txtPointers.ReadOnly = true;
+            this.txtPointers.Size = new System.Drawing.Size(62, 310);
+            this.txtPointers.TabIndex = 0;
+            this.txtPointers.Text = "";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCode.Location = new System.Drawing.Point(0, 0);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(517, 310);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.Text = "";
+            this.txtCode.TextChanged += new System.EventHandler(this.txtCode_TextChanged);
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitter);
+            this.Name = "TextEditor";
+            this.Size = new System.Drawing.Size(583, 310);
+            this.splitter.Panel1.ResumeLayout(false);
+            this.splitter.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).EndInit();
+            this.splitter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitter;
+        private System.Windows.Forms.RichTextBox txtPointers;
+        private System.Windows.Forms.RichTextBox txtCode;
+    }
+}
---
+++ DODOSembly/IDE/TextEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDE {
+    public partial class TextEditor : UserControl {
+        public TextEditor() {
+            InitializeComponent();
+        }
+
+        private void txtCode_TextChanged(object sender, EventArgs e) {
+            var v = txtCode.Lines.ToList().Select((s)=>k(s));
+            Enumerable.Range(0, txtCode.Text.Length).ToList().ForEach((index) => {
+                m(index,v);
+            });
+        }
+        private int k(string s) {
+            return s.Length;
+        }
+        private void m(int index,IEnumerable<int> v ) {
+            while(index < txtPointers.Lines.Length) { txtPointers.Text += "\r\n"; }
+            txtPointers.Lines[index] = Convert.ToString(v.Take(index).Aggregate((n1, n2) => n1 + n2));
+        }
+    }
+}
